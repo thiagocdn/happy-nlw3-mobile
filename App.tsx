57f22f6a2ede896518304ea7a30a68a179336095
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-10-18 13:41:53 -0300

    Added marker on map

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/images/map_marker.png';
 
 export default function App() {
   return (
@@ -15,7 +17,15 @@ export default function App() {
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
           }}
-        />
+        >
+          <Marker 
+            icon={mapMarker}
+            coordinate={{
+              latitude: -22.9722666,
+              longitude: -46.9902287
+            }}
+          />
+        </MapView>
     </View>
   );
 }
