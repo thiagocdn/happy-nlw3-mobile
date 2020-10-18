76e0689c278ece7b78e51a2de80584ec7a93f0d0
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-10-18 14:07:52 -0300

    Added Footer + Fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
 import mapMarker from './src/images/map_marker.png';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if(!fontsLoaded){
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <MapView
@@ -20,16 +32,29 @@ export default function App() {
         >
           <Marker 
             icon={mapMarker}
+            calloutAnchor={{
+              x: 2.7,
+              y: 0.8,
+            }}
             coordinate={{
               latitude: -22.9722666,
               longitude: -46.9902287
             }}
           >
-            <Callout>
-              <Text>Lar das Meninas</Text>
+            <Callout tooltip onPress={() => {alert('oi')}}>
+              <View style={styles.calloutContainer} >
+                <Text style={styles.calloutText} >Lar das Meninas</Text>
+              </View>
             </Callout>
           </Marker>
         </MapView>
+        
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>2 Orfanatos encontrados</Text>
+          <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+            <Feather name="plus" size={20} color='#FFF' />
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
@@ -42,5 +67,60 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height
-  }
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089A5',
+    fontSize: 14,
+    fontFamily: 'Nunito_700Bold',
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+    
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3,
+
+    shadowColor: 'rgba(0,0,0,1)',
+    shadowRadius: 3,
+    shadowOffset: { height: 2, width: 2 },
+    shadowOpacity: 0.5
+  },
+
+  footerText: {
+    fontFamily: 'Nunito_700Bold',
+    color: '#8FA7B3',
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15C3D6',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+
 });
